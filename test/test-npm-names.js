87fb8782dedf8b9401131d3b9408ceb9de7ab984
Mora <qiuzhongleiabc@126.com>
2016-06-12 10:31:33 +0800

    refactor

--- test/test-npm-names.js
+++ test/test-npm-names.js
@@ -1,10 +1,16 @@
-import test from 'ava';
-import assert from 'assert';
-
+const assert = require('assert');
 const names = require('../build/npm-names');
 
-test('npm-names', t => {
-  assert(Array.isArray(names), 'is an array');
-  assert(names.length > 260 * 1000, 'has hella names');
-  t.pass();
+describe('npm-names', function () {
+  it('is an array', function () {
+    assert(Array.isArray(names), 'is an array');
+  });
+
+  it('contains jquery', function () {
+    assert(names.indexOf('jquery') > 0, 'cantains jquery');
+  });
+
+  it('has hella names', function () {
+    assert(names.length > 260 * 1000, 'has hella names');
+  });
 });
